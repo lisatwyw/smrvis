a752de50038a9ebd215c90194f6d24d63f58e60d
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-05-26 20:29:14 -0700

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,18 @@
+
+import os
+
+file='IT5_IV5_EN0_IR3_ARseunet_NF16_KS-1_NR-1_NC2_LSbfce_OPadam_LR0.0005_BS64_WT1.0_CR3_SE2_RT50_DO0.2_NZ3_ACsigmoid_ST10_SH1'
+
+if 'ckpt' in file:
+    ff=os.path.basename(file[:-5]).split('_')
+else:
+    ff=os.path.basename(file).split('_')
+for s in ff:
+    try:
+        exec( '%s=%s' %( s[:2], s[2:] ) )
+        print( '%s=%s' %( s[:2], s[2:] ) )
+    except:
+        exec( '%s=\'%s\'' %( s[:2], s[2:] ) )
+        print( '%s=\'%s\'' %( s[:2], s[2:] ) )
+        
+NX=NY=266; NZ=3; exec( open('c0args.py').read() ); exec( open('c1tfsetup.py' ).read())
