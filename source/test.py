0c2c3810d8a8c96874108dc987a2b6d6cd775331
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-05-29 14:53:17 -0700

    Update test.py

--- source/test.py
+++ source/test.py
@@ -2,3 +2,11 @@ import platform; print(platform.platform())
 import sys; print("Python", sys.version)
 import tensorflow as tf; print("Tensorflow", tf.__version__)
 
+
+# https://www.activestate.com/resources/quick-reads/how-to-list-installed-python-packages/
+import pkg_resources
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+   for i in installed_packages])
+
+print(installed_packages_list)
