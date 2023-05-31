367793c8603babe72ecdcafb57324c76113af572
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-05-30 17:14:13 -0700

    Update test.py

--- source/test.py
+++ source/test.py
@@ -12,6 +12,17 @@ installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
 print(installed_packages_list)
 
 
+print('Testing')
+
+
+import SimpleITK as sitk
+from tqdm import tqdm
 
 model = tf.keras.models.load_model( '../models/IT3_IV2_EN0_IR3_ARunetpp_NF.h5')
-print(model.summary())
+print( model.summary() )
+
+ctn =1
+try:
+   us_filename=sys.argv[ctn]
+except:
+   print('No filename provided')
